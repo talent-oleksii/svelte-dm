02a6910a3a898884cceb1fe2fb143c2cc0d787bc
dgodfredson <oleksiikaravanov@gmail.com>
2023-09-27 15:29:14 -0400

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,145 +1,252 @@
 <script>
-  import { DataTable, Pagination, Search } from "carbon-components-svelte";
+  import { DataTable, PaginationNav, Toolbar, ToolbarContent, Search, ToolbarSearch, Select, SelectItem, DataTableSkeleton } from "carbon-components-svelte";
   import {onMount} from "svelte";
+  import './main.scss';
 
 
-  let rows = [
-    {
-      id: 14,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    },
-    {
-      id: 193,
-      table_name: "BIAST ASSETNUMSYS",
-      schema: "landing-zone-ces",
-      database_name: "ETL",
-      source_system: "CES",
-      active: "Y"
-    },
-    {
-      id: 194,
-      table_name: "BiPRK FP RATING",
-      schema: "landing-zone-ces",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    },
-    {
-      id: 523,
-      table_name: "CBPOR ENTITY LINK",
-      schema: "landing-zone-ces",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "N"
-    },
-    {
-      id: 279,
-      table_name: "CES BIAST ASSETNUMSYS",
-      schema: "dlt",
-      database_name: "ETL",
-      source_system: "CES",
-      active: "N"
-    },
-    {
-      id: 236,
-      table_name: "CES BIAST ASSETNUMSYS",
-      schema: "cmp",
-      database_name: "ETL",
-      source_system: "CES",
-      active: "Y"
-    },
-    {
-      id: 633,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    },
-    {
-      id: 721,
-      table_name: "ANIMASTER",
-      schema: "cmp",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "N"
-    },
-    {
-      id: 543,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    },
-    {
-      id: 111,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "CES",
-      active: "Y"
-    },
-    {
-      id: 114,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    },
-    {
-      id: 134,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    },
+  // let rows = [
+  //   {
+  //     id: 14,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 193,
+  //     table_name: "BIAST ASSETNUMSYS",
+  //     schema: "landing-zone-ces",
+  //     database_name: "ETL",
+  //     source_system: "CES",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 194,
+  //     table_name: "BiPRK FP RATING",
+  //     schema: "landing-zone-ces",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 523,
+  //     table_name: "CBPOR ENTITY LINK",
+  //     schema: "landing-zone-ces",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "N"
+  //   },
+  //   {
+  //     id: 279,
+  //     table_name: "CES BIAST ASSETNUMSYS",
+  //     schema: "dlt",
+  //     database_name: "ETL",
+  //     source_system: "CES",
+  //     active: "N"
+  //   },
+  //   {
+  //     id: 236,
+  //     table_name: "CES BIAST ASSETNUMSYS",
+  //     schema: "cmp",
+  //     database_name: "ETL",
+  //     source_system: "CES",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 633,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 721,
+  //     table_name: "ANIMASTER",
+  //     schema: "cmp",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "N"
+  //   },
+  //   {
+  //     id: 543,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 111,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "CES",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 114,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 134,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   },
 
-    {
-      id: 154,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    },
-    {
-      id: 164,
-      table_name: "ANIMASTER",
-      schema: "landing-zone-pr",
-      database_name: "ETL",
-      source_system: "PR",
-      active: "Y"
-    }
-  ];
+  //   {
+  //     id: 154,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   },
+  //   {
+  //     id: 164,
+  //     table_name: "ANIMASTER",
+  //     schema: "landing-zone-pr",
+  //     database_name: "ETL",
+  //     source_system: "PR",
+  //     active: "Y"
+  //   }
+  // ];
+  let rows = [];
 
-  let pageSize = 15;
+  let pageSize = 25;
   let page = 1;
-  onMount(() => {
-    loadData();
+  let filteredRowIds = [];
+  let sourceData = [];
+  let totalCount;
+  let totalPage = 0;
+  let selected = 25;
+  let sourceid;
+  let isActive = 2;
+  let value = "";
+
+  onMount(async () => {
+    sourceData = await loadSourceName();
+    rows = await loadData();
   });
-  async function loadData() {
+  async function loadSourceName() {
+    let result = [];
     try {
-      const response = await fetch('https://webconfighq.azurewebsites.net/api/GetInputs?code=9gt1jvz6pB_fgMPHcGQbyNvQSHhyvd9wgIbFek3hQpxfAzFu5gM7ig==', {
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Atlassian-Token': 'no-check',
-        },
-      });
+      const response = await fetch('https://webconfighq.azurewebsites.net/api/GetSourceSystems?code=1qZ3OS-Znv0D3DdpvtGzuacY3U0j7XAfDxnuT-tQa7sFAzFuo2u4HQ==');
       if (!response.ok) {
         throw new Error('Request failed');
       }
       const data = await response.json();
-      console.log("data=",data)
+      for( let i = 0; i < data.Results.length; i++) {
+        result.push({
+          source_id: data.Results[i].source_id,
+          source_name: data.Results[i].source_name
+        });
+      }
     } catch (error) {
       console.error(error);
     }
+    return result;
+  }
+  async function loadData() {
+    let result = [];
+    try {
+      let fetchUrl = 'https://webconfighq.azurewebsites.net/api/GetInputs?code=9gt1jvz6pB_fgMPHcGQbyNvQSHhyvd9wgIbFek3hQpxfAzFu5gM7ig==&';
+      fetchUrl += `pagelength=${pageSize}&`;
+      fetchUrl += `pageid=${page}`;
+      if(sourceid > 0 ) {
+        fetchUrl += `&sourceid=${sourceid}`;
+      }
+      if( isActive < 2 ) {
+        fetchUrl += `&isactive=${isActive}`;
+      }
+      if(value.length > 0) {
+        fetchUrl += `&search=${value}`;
+      }
+      const response = await fetch(fetchUrl);
+      if (!response.ok) {
+        throw new Error('Request failed');
+      }
+      const data = await response.json();
+      for( let i = 0; i < data.Results.length; i++) {
+        const source_name = getSourceNameById(data.Results[i].source_id);
+        result.push({
+          id: data.Results[i].id,
+          table_name: data.Results[i].table_name,
+          schema: data.Results[i].table_schema,
+          database_name: data.Results[i].database_name,
+          source_system: source_name,
+          source_id: data.Results[i].source_id,
+          active: data.Results[i].is_active ? 'Y' : 'N'
+        });
+      }
+      if( data.Results.length == 0 ) {
+        result.push({
+          id: "",
+          table_name: "",
+          schema: "",
+          database_name: "",
+          source_system: "",
+          source_id: "",
+          active: ""
+        });
+      }
+      totalCount = data.Count;
+      totalPage = data.PageCount;
+    } catch (error) {
+      console.error(error);
+    }
+    return result;
+  }
+  function getSourceNameById(source_id) {
+    for( let i = 0; i < sourceData.length;i++) {
+      if(sourceData[i].source_id == source_id) return sourceData[i].source_name;
+    }
+    return '';
+  }
+  async function changePageSize(value) {
+    rows = [];
+    pageSize = value;
+    page = 1;
+    rows = await loadData();
+  }
+  async function changePage() {
+    rows = [];
+    rows = await loadData();
+  }
+  async function changeSystemOpt(value) {
+    rows = [];
+    sourceid = getSourceIdByName(value);
+    rows = await loadData();
+  }
+  async function changeActiveOpt(value) {
+    rows = [];
+    isActive = value == 'Y' ? 1 : 0;
+    if( value == "Active") isActive = 2;
+    rows = await loadData();
+  }
+  async function searchText(e) {
+    if(e.key != "Enter") return;
+    rows = [];
+    rows = await loadData();
+  }
+  async function searchFullText(e) {
+    value = "";
+    rows = [];
+    rows = await loadData();
+  }
+  function getSourceIdByName(name) {
+    for( let i = 0; i < sourceData.length;i++) {
+      if(sourceData[i].source_name == name) return sourceData[i].source_id;
+    }
+    return 0;
   }
 </script>
 
@@ -150,69 +257,85 @@
 
 <section>
   <h4 class="w-fit pt-6 mb-5">Table List</h4>
-  <div class="mb-5 flex justify-between">
-    <div class="flex">
-      <Search />
-      <select
-        name="source_system"
-        id="source_system"
-        class="rounded ml-3 border-2 border-[#ecedee] bg-[#f4f4f4]"
-      >
-        <option value="blank">Source System</option>
-        <option value="PR">PR</option>
-        <option value="CES">CES</option>
-      </select>
-      <select
-        name="schema"
-        id="schema"
-        class="rounded ml-3 border-2 border-[#ecedee] bg-[#f4f4f4]"
-      >
-        <option value="blank">Schema</option>
-        <option value="PR">PR</option>
-        <option value="CES">CES</option>
-      </select>
-      <select
-        name="active"
-        id="active"
-        class="rounded ml-3 border-2 border-[#ecedee] bg-[#f4f4f4]"
-        value="CES"
-      >
-        <option value="blank">Active</option>
-        <option value="PR">PR</option>
-        <option value="CES">CES</option>
-      </select>
-    </div>
-    <div class="flex">
-      <p>showing</p>
-      <select
-        name="page_amount"
-        id="page_amount"
-        class="rounded ml-3 border-2 border-[#ecedee] bg-[#f4f4f4] h-fit text-sm py-0 pr-2"
-        bind:value={pageSize}
-      >
-        <option value={5}>5</option>
-        <option value={10}>10</option>
-        <option value={15}>15</option>
-      </select>
-      <p>of 60 results</p>
-    </div>
-  </div>
+  {#if rows.length < 1}
+    <DataTableSkeleton />
+  {:else}
+    <DataTable
+      headers={[
+        { key: "id", value: "id" },
+        { key: "table_name", value: "table name" },
+        { key: "schema", value: "schema" },
+        { key: "database_name", value: "database name" },
+        { key: "source_system", value: "source system" },
+        { key: "active", value: "active" }
+      ]}
 
-  <DataTable
-    sortable
-    class="w-full"
-    headers={[
-      { key: "id", value: "id" },
-      { key: "table_name", value: "table name" },
-      { key: "schema", value: "schema" },
-      { key: "database_name", value: "database name" },
-      { key: "source_system", value: "source system" },
-      { key: "active", value: "active" }
-    ]}
-    {pageSize}
-    {page}
-    {rows}
-  />
+      {pageSize}
+      {rows}
+    >
+        <Toolbar class="mb-8">
+          <ToolbarContent class="flex-start">
+            <div class="flex justify-between w-full">
+              <div class="flex">
+                  <Search
+                    bind:value
+                    class="!w-[325px]"
+                    on:keydown={async (e)=> searchText(e)}
+                    on:click={async (e)=> searchFullText(e)}
+                />
+                <div>
+                  <Select 
+                    inline
+                    selected = {getSourceNameById(sourceid)}
+                    on:update={async (e)=> changeSystemOpt(e.detail)}
+                  >
+                    <SelectItem value="Source System" />
+                    {#each sourceData as data, index}
+                      <SelectItem value={data.source_name} />
+                    {/each}
+                  </Select>
+                </div>
+                <div>
+                  <Select 
+                    inline
+                    selected={isActive == 2 ? "Active" : isActive == 1? "Y" : "N"}
+                    on:update={async (e)=> changeActiveOpt(e.detail)}
+                  >
+                    <SelectItem value="Active" />
+                    <SelectItem value="Y" />
+                    <SelectItem value="N" />
+                  </Select>
+                </div>
+              </div>
+              
+              <div class="flex items-center">
+                <span class="mr-2">Showing</span>
+                <Select 
+                  inline 
+                  size="sm" 
+                  class="mr-4"
+                  selected={pageSize}
+                  on:update={async (e)=> changePageSize(e.detail)}
+                >
+                  <SelectItem value={25}/>
+                  <SelectItem value={50}/>
+                  <SelectItem value={75}/>
+                  <SelectItem value={100}/>
+                </Select>
+                <span>of {totalCount} results</span>
+              </div>
+            </div>
+
+          </ToolbarContent>
+        </Toolbar>
+    </DataTable>
+    <PaginationNav 
+      shown={3} 
+      total={totalPage} 
+      bind:page 
+      on:change={async (e) => changePage()}
+    />
+  {/if}
 </section>
 
 <style>
@@ -252,18 +375,8 @@
     background-color: white;
   }
 
-  /* Search Button Style */
-
-  :global(.bx--search) {
-    width: 60%;
-  }
-
-  :global(
-      .bx--search--xl .bx--search-input,
-      .bx--search--xl.bx--search--expandable.bx--search--expanded
-        .bx--search-input
-    ) {
-    border-radius: 4px;
-    border: 2px solid #ecedee;
+  /* Toolbar Content Style*/
+  :global(.bx--toolbar-content) {
+    justify-content: flex-start !important;
   }
 </style>
